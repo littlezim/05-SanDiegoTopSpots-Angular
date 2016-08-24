27729e5f9bdb3c8ce3cdab918fa79080b47f1cf7
littlezim <littlezim18@gmail.com>
2016-08-24 15:41:02 -0700

    Created API For Angular

--- index.js
+++ index.js
@@ -3,19 +3,7 @@
 	angular.module('app', []);
 
 	angular.module('app').controller("mainCtrl", function($scope, $http){
-		$http.get("topspots.json").then(function(response){
+		$http.get("http://localhost:61184/api/TopSpots").then(function(response){
 			$scope.bestSpots = response.data;
 		})
-		
 	});
-		angular.module('myApplicationModule', ['uiGmapgoogle-maps']).config(function(uiGmapGoogleMapApiProvider) {uiGmapGoogleMapApiProvider.configure({
-          	
-       			 v: '3.20', //defaults to latest 3.X anyhow
-        		libraries: 'weather,geometry,visualization'
-
-    		});
-		});
-
-
-		
-
